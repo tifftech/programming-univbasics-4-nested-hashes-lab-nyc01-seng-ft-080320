442f4b81df4800c1cc1df80ab46174f64ab7aea8
Tiffani Best <tiffanimelissabest@gmail.com>
2020-07-31 03:46:43 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,6 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
+  return programmer_hash[:grace_hopper]  
 
 end
 
@@ -36,6 +37,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+  return programmer_hash[:alan_kay][:known_for]  
 
 end
 
@@ -55,19 +57,12 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+  return programmer_hash[:dennis_ritchie][:languages][0]  
 
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
-	programmer_hash = 
- 		{
+  programmer_hash = 
       :grace_hopper => {
         :known_for => "COBOL",
         :languages => ["COBOL", "FORTRAN"]
@@ -81,7 +76,6 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
 end
 
 def changing_alan
@@ -103,7 +97,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     return programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +121,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+     return programmer_hash
 
 end
